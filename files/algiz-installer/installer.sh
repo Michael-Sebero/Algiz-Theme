9ceb9ee76243b0fa9b9216aa2ffd79b60323a561
Michael Sebero <106393611+Michael-Sebero@users.noreply.github.com>
2026-09-19 16:15:29 -0400

    Update installer.sh

--- files/algiz-installer/installer.sh
+++ files/algiz-installer/installer.sh
@@ -9,33 +9,39 @@ echo "2. LAPTOP"
 
 read -p "Enter your choice (1-2): " choice
 
-### CLONE ALGIZ THEME FILES ###
+### INSTALL NEEDED XFCE PACKAGES ###
 
-echo -e "\e[1mCloning Algiz Theme files...\e[0m"
-mkdir -p /home/algiz-files/
-git clone https://github.com/Michael-Sebero/Algiz-Theme /home/algiz-files/
-cd /home/algiz-files/files/algiz-packages/
+if pacman -Qq | grep -q "^thunar$"; then
+    pacman -Sy --noconfirm mugshot xfce4-panel-profiles lightdm-gtk-greeter-settings
 
-# DESKTOP SELECTION
-if [ "$choice" = "1" ]; then
-  unzip -o algiz-dotfiles-desktop.zip -d /home/$USER/
-  unzip -o algiz-root-main.zip -d /
+    ### CLONE ALGIZ THEME FILES ###
+
+    echo -e "\e[1mCloning Algiz Theme files...\e[0m"
+    mkdir -p /home/algiz-files/
+    git clone https://github.com/Michael-Sebero/Algiz-Theme /home/algiz-files/
+    cd /home/algiz-files/files/algiz-packages/
+
+    # DESKTOP SELECTION
+    if [ "$choice" = "1" ]; then
+      unzip -o algiz-dotfiles-desktop.zip -d /home/$USER/
+      unzip -o algiz-root-main.zip -d /
+    fi
+
+    # LAPTOP SELECTION
+    if [ "$choice" = "2" ]; then
+      unzip -o algiz-dotfiles-laptop.zip -d /home/$USER/
+      unzip -o algiz-root-main.zip -d /
+    fi
+
+    ### LAST COMMANDS ###
+
+    # RESET PERMISSIONS
+    reset-permissions
+
+    # CLEANUP
+    cd /
+    rm -rf /home/algiz-files/
+    echo -e "\e[1mAlgiz Theme dotfiles have been successfully extracted\e[0m"
+    reboot
 fi
-
-# LAPTOP SELECTION
-if [ "$choice" = "2" ]; then
-  unzip -o algiz-dotfiles-laptop.zip -d /home/$USER/
-  unzip -o algiz-root-main.zip -d /
-fi
-
-### LAST COMMANDS ###
-
-# RESET PERMISSIONS
-reset-permissions
-
-# CLEANUP
-cd /
-rm -rf /home/algiz-files/
-echo -e "\e[1mAlgiz Theme dotfiles have been successfully extracted\e[0m"
-reboot
 '
