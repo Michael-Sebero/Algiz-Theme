c3c25968eeee89e92d2394f963509df13bbe8582
Michael Sebero <106393611+Michael-Sebero@users.noreply.github.com>
2026-09-22 10:51:12 -0400

    Update installer.sh

--- files/algiz-installer/installer.sh
+++ files/algiz-installer/installer.sh
@@ -9,36 +9,34 @@ echo "2. LAPTOP"
 
 read -p "Enter your choice (1-2): " choice
 
-### INSTALL NEEDED XFCE PACKAGES ###
+### CLONE ALGIZ THEME FILES ###
+
+echo -e "\e[1mCloning Algiz Theme files...\e[0m"
+mkdir -p /home/algiz-files/
+git clone https://github.com/Michael-Sebero/Algiz-Theme /home/algiz-files/
+cd /home/algiz-files/files/algiz-packages/
+
+### REMOVE OLD XFCE DIRECTORIES ###
+
+echo -e "\e[1mRemoving old xfce directories from /home/$USER...\e[0m"
+find /home/$USER -maxdepth 1 -type d -iname "*xfce*" -exec rm -rf {} +
+
+# DESKTOP SELECTION
+if [ "$choice" = "1" ]; then
+  unzip -o algiz-dotfiles-desktop.zip -d /home/$USER/
+  unzip -o algiz-root-main.zip -d /
+fi
+
+# LAPTOP SELECTION
+if [ "$choice" = "2" ]; then
+  unzip -o algiz-dotfiles-laptop.zip -d /home/$USER/
+  unzip -o algiz-root-main.zip -d /
+fi
+
+### XFCE ###
 
 if pacman -Qq | grep -q "^thunar$"; then
     pacman -Sy --noconfirm mugshot xfce4-panel-profiles lightdm-gtk-greeter-settings artix-dark-theme
-
-    ### CLONE ALGIZ THEME FILES ###
-
-    echo -e "\e[1mCloning Algiz Theme files...\e[0m"
-    mkdir -p /home/algiz-files/
-    git clone https://github.com/Michael-Sebero/Algiz-Theme /home/algiz-files/
-    cd /home/algiz-files/files/algiz-packages/
-
-    ### REMOVE OLD XFCE DIRECTORIES ###
-
-    echo -e "\e[1mRemoving old xfce directories from /home/$USER...\e[0m"
-    find /home/$USER -maxdepth 1 -type d -iname "*xfce*" -exec rm -rf {} +
-
-    # DESKTOP SELECTION
-    if [ "$choice" = "1" ]; then
-      unzip -o algiz-dotfiles-desktop.zip -d /home/$USER/
-      unzip -o algiz-root-main.zip -d /
-    fi
-
-    # LAPTOP SELECTION
-    if [ "$choice" = "2" ]; then
-      unzip -o algiz-dotfiles-laptop.zip -d /home/$USER/
-      unzip -o algiz-root-main.zip -d /
-    fi
-
-    ### LAST COMMANDS ###
 
     # RESET PERMISSIONS
     reset-permissions
@@ -48,11 +46,11 @@ if pacman -Qq | grep -q "^thunar$"; then
     xfwm4 --replace &
     xfce4-panel -r
     xfdesktop --reload
-
-    # CLEANUP
-    cd /
-    rm -rf /home/algiz-files/
-    echo -e "\e[1mAlgiz Theme dotfiles have been successfully extracted\e[0m"
-    reboot
 fi
+
+### CLEANUP ###
+cd /
+rm -rf /home/algiz-files/
+echo -e "\e[1mAlgiz Theme dotfiles have been successfully extracted\e[0m"
+reboot
 '
